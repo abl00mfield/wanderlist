f0c03fb9e448a6071f3c6803e63c5c66162cb341
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-17 11:07:11 -0600

    mounted custom middleware and refactored root route to not pass user object

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const express = require('express');
+//set up dependencies
+const dotenv = require("dotenv");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+//custom middleware
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
-const authController = require('./controllers/auth.js');
+dotenv.config();
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+//set up controllers
+const authController = require("./controllers/auth.js");
 
+//set up port
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+//connect to database
 mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+//mount middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    //this stores the session in the database so it won't be lost
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
   })
 );
+app.use(passUserToView);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
-app.use('/auth', authController);
+app.use("/auth", authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
