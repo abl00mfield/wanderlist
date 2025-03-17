8418d4a118138f53d6b53fe7ea248ece5b1cc1d5
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-17 15:04:22 -0600

    wip:  added destination reference to user schema

--- models/user.js
+++ models/user.js
@@ -1,16 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
   },
+
   password: {
     type: String,
     required: true,
   },
+
+  destinationIds: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Destination",
+    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
